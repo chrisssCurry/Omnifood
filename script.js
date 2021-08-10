acc118c3a58b1620fea60c54de57da3dc61c5161
chrisssCurry <cristi_cr47@yahoo.com>
2021-08-10 19:54:02 +0300

    modified animations, images, added leaflet.js

--- script.js
+++ script.js
@@ -1,108 +1,183 @@
-/*the script will only load AFTER the DOM is loaded*/
-$(document).ready(function() {
-    /*Sticky nav*/
-    $('.js--section-features').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('nav').addClass('sticky');
-        } else {
-            $('nav').removeClass('sticky');
-        }
-    }, {
-        offset: '60px;'
-    });
+'use strict';
 
-    /*Scroll on buttons*/ 
-    $('.js--scroll-plans').click(function() {
-        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
-    });
 
-    $('.js--scroll-features').click(function() {
-        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);
-    });
+//----------------DOM ELEMENTS-------------------
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
+const features = document.getElementById('features--1');
+const mealsRow1 = document.getElementById('meals-row--1');
+const mealsRow2 = document.getElementById('meals-row--2');
+const steps = document.querySelector('.steps-box');
+const cities = document.querySelector('.cities-row');
+const premiumPlan = document.querySelector('.premium');
+const animatedElements = [features, mealsRow1, mealsRow2, steps, cities];
 
-    $('.logo-black').click(function(){
-        $('html, body').animate({scrollTop: $('html').offset().top}, 1000);
-    });
 
-/*Nav scroll*/
-    $('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          }
-        });
-      }
+//----------------STICKY NAVIGATION-------------------
+
+//Intersection Observer API
+const navHeight = nav.getBoundingClientRect().height;
+// const cookieHeight = cookieMessage.getBoundingClientRect().height;
+
+//Gets called everytime the observed object (header)
+//is intersecting the obsOptions.root element at the obsOptions.threshold specified
+const obsCallback = function(entries, observer) {
+  entries.forEach(entry => {
+    //If the header is not showing in the viewport, add the sticky class to the nav
+    if (!entry.isIntersecting) {
+      nav.classList.add('sticky');
+    //Remove sticky class as soon as the header shows in the viewport  
+    } else {
+      nav.classList.remove('sticky');
     }
   });
+}
 
-  /*Animations on scroll*/
+const obsOptions = {
+  root: null, //if null => root: the whole viewport
+  threshold: 0, //0%
+  rootMargin: `${-navHeight - 10}px`//the height of the nav = 90px (whenever there is only 90px remaining, before the header disappears from the viewport, the nav automatically becomes sticky and covers that blank space)
+}
 
-$('.js--waypoint-1').waypoint(function(direction) {
-    $('.js--waypoint-1').addClass('animate__animated animate__backInUp')
-  }, {
-    offset: '60%'
+const headerObserver = new IntersectionObserver(obsCallback, obsOptions);
+headerObserver.observe(header);
+
+
+
+
+//----------------SCROLL TO ANY SECTION-------------------
+
+//1.Add event listener to common parent element
+document.querySelector('.main-nav').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  //2.Check if the event was triggered by the parent or the child element (by the class names)
+  //No elements inside the buttons so no need to use closest()
+  if (e.target.classList.contains('nav__link')) {
+    //3. Determine which child element started the event
+    const id = e.target.getAttribute('href');
+    const section = document.querySelector(`${id}`);
+    section.scrollIntoView({behavior: 'smooth'});
+  }
 });
 
 
-$('.js--waypoint-2').waypoint(function(direction) {
-  $('.js--waypoint-2').addClass('animate__animated animate__fadeInUp')
-}, {
+
+//----------------REVEAL SECTIONS ON SCROLL-------------------
+
+//Hide elements before triggering their animations
+animatedElements.forEach(element => element.classList.add('hidden'));
+
+
+const featuresWaypoint = new Waypoint({
+  element: features,
+  handler: function(direction) {
+    // this.element.classList.remove('hidden');
+    this.element.classList.add('animate__animated', 'animate__backInUp');
+  },
+  offset: '75%'
+})
+
+
+//Meals section animation
+const mealsWaypoint = new Waypoint({
+  element: mealsRow1,
+  handler: function() {
+    this.element.classList.remove('hidden');
+    this.element.classList.add('animate__animated', 'animate__fadeInLeft');
+    mealsRow2.classList.remove('hidden');
+    mealsRow2.classList.add('animate__animated', 'animate__fadeInRight')
+  },
+  offset: '75%'
+});
+
+
+//Steps section animation
+const stepsWaypoint = new Waypoint({
+  element: steps,
+  handler: function() {
+    this.element.classList.remove('hidden');
+    this.element.classList.add('animate__animated', 'animate__fadeInUp');
+  },
+  offset: '75%'
+});
+
+
+//Cities section animation
+const citiesWaypoint = new Waypoint({
+  element: cities,
+  handler: function() {
+    this.element.classList.remove('hidden');
+    this.element.classList.add('animate__animated', 'animate__fadeIn');
+  },
   offset: '60%'
 });
 
 
-$('.js--waypoint-3').waypoint(function(direction) {
-  $('.js--waypoint-3').addClass('animate__animated animate__fadeIn')
-}, {
-  offset: '60%'
+//Plans section animation
+const premiumWaypoint = new Waypoint({
+  element: premiumPlan,
+  handler: function() {
+    this.element.classList.remove('hidden');
+    this.element.classList.add('animate__animated', 'animate__bounce');
+  },
+  offset: '50%'
 });
 
 
-$('.js--waypoint-4').waypoint(function(direction) {
-  $('.js--waypoint-4').addClass('animate__animated animate__bounce')
-}, {
-  offset: '60%'
-});
+
+//----------------LEAFLET.JS MAP-------------------
+const coordinatesView = [48.75744276680307, 25.941039555340756];
+let zoom = 4.5;
+const map = L.map('map', {minZoom: 3}).setView(coordinatesView, zoom);
 
 
-$('.js--waypoint-5').waypoint(function(direction) {
-  $('.js--waypoint-5').addClass('animate__animated animate__zoomIn')
-}, {
-  offset: '80%'
-});
-    
+//Map style(the tile layer)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
 
-/*Mobile nav*/
+
+//Class to build new markers
+class Marker {
+  constructor(latitude, longitude, popupMessage, popupImage) {
+    this.coordinates = [latitude, longitude];
+    this.popupMessage = popupMessage;
+    this.popupImage = popupImage;
+
+    //Add market to the L (leaflet) object with the values passed in the constructor
+    L.marker([latitude, longitude])
+            .addTo(map)
+            .bindPopup(L.popup({
+                minWidth:100,
+                minHeight:100,
+                autoClose: false,
+                className: 'leaflet-popup'
+            }))
+            .setPopupContent(`
+            <div style="text-align:center">
+                <img style  src=${this.popupImage}></img>
+            </div> `)
+            .openPopup();
+  }
+}
+
+//Markers on the map for all 4 cities
+const markerLondon = new Marker(51.498348717745834, -0.14022749093375764, 'Omnifood London', "/london.jpg");
+const markerRome = new Marker(41.906025696107086, 12.45999457597215, 'Omnifood Rome', "/rome.jpg");
+const markerBerlin = new Marker(52.51113649167234, 13.387582290054013, 'Omnifood Berlin', "/berlin.jpg");
+const markerParis = new Marker(48.856998922645765, 2.292056960898533, 'Omnifood Paris', "/paris.jpg");
+
+
+
+//----------------MOBILE NAV-------------------
 $('.js--nav-icon').click(function() {
   let nav = $('.js--main-nav');
-  let icon =$('.js--nav-icon');
-
+  let icon = $('.js--nav-icon');
   nav.slideToggle(500);
 
-  if (icon.attr('name')==='menu') {
+  if (icon.attr('name') === 'menu') {
     icon.attr('name','close');
   } else {
     icon.attr('name','menu');
@@ -110,5 +185,8 @@ $('.js--nav-icon').click(function() {
   
 });
 
-});
 
+//----------------FOOTER YEAR-------------------
+let currentYear = new Date().getFullYear();
+const footerCopyright = document.querySelector('.footer__copyright');
+footerCopyright.textContent = `\u00A9 Copyright ${currentYear} by Omnifood. All rights reserved.`
